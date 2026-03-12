2aa2ea083884ad67364d7ddf0e97a254dbeba44a
poeticalbyte <poeticalbyte@gmail.com>
2026-03-12 03:14:23 -0500

    fix: change api url

--- hajsu-frontend/src/services/AttendeeService.ts
+++ hajsu-frontend/src/services/AttendeeService.ts
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:8080/api/attendees";
+
+const API_URL = "https://hajsu-etnomoda-event-platform.onrender.com/api/attendees";
 
 export async function registerAttendee(attendee: { fullName: string; email: string; country: string; city: string; institution: string }) {
   const response = await fetch(API_URL, {
